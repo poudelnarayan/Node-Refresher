eeece34f2823c388968251640413f2df9f765dd5
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 16:35:10 +0545

    outputting as plain text

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   console.log("INCOMING REQUEST");
   console.log(req.method, req.url);
 
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Content-Type", "text/plain");
   res.end("<h1>Succes!</h1>"); // -> outputs <h1>Success!</h1> in the browser
 });
 
