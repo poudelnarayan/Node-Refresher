93da355fe70f55f5336894124f1b4301f5cf7343
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 16:31:42 +0545

    sending html files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   console.log("INCOMING REQUEST");
   console.log(req.method, req.url);
 
-  res.end("Succes!");
+  res.end("<h1>Succes!</h1>");
 });
 
 server.listen(5000);
